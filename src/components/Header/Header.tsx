2980632a9ff49aa05eee5eec19fae3d661a6fcd5
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-03-25 04:33:32 -0300

    add styles to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { 
+    AppBar, 
+    Toolbar, 
+    Typography, 
+    InputBase, 
+    Box 
+} from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+
+import useStyles from './styles';
 
 const Header = () => {
+    const classes = useStyles();
     return (
-        <h1>Header</h1>
+        <AppBar 
+            className={classes.primaryColor} 
+            position='static'
+        >
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <Search/>
+                            </div>
+                                <InputBase placeholder="Explore new places..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
