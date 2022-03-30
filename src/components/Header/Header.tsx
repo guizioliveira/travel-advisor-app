e14168af7cb33a21cc2e08e7df21abff885f7c1d
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-03-30 00:39:25 -0300

    intregation with Google Place API

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,51 @@
-import React from 'react';
-import { Autocomplete } from '@react-google-maps/api';
-import { 
-    AppBar, 
-    Toolbar, 
-    Typography, 
-    InputBase, 
-    Box 
-} from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+import React, { useState } from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import GoogleMapReact from "google-map-react";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
-const Header = () => {
-    const classes = useStyles();
-    return (
-        <AppBar 
-            className={classes.primaryColor} 
-            position='static'
-        >
-            <Toolbar className={classes.toolbar}>
-                <Typography variant='h5' className={classes.title}>
-                    Travel Advisor
-                </Typography>
-                <Box display="flex">
-                    {/* <Autocomplete> */}
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon/>
-                            </div>
-                                <InputBase placeholder="Explore new places..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
-                        </div>
-                    {/* </Autocomplete> */}
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+interface HeaderProps {
+  setCoordinates: (coord: GoogleMapReact.Coords) => void;
 }
+
+const Header: React.FC<HeaderProps> = ({ setCoordinates }) => {
+  const classes = useStyles();
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng });
+  };
+
+  return (
+    <AppBar className={classes.primaryColor} position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                id="searchInput"
+                placeholder="Explore new places..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
