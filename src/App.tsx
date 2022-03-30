98d9faea954e42c9e607a75c70deb3d2a71eb98f
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-03-30 00:47:40 -0300

    fix refreach position

--- src/App.tsx
+++ src/App.tsx
@@ -31,15 +31,15 @@ function App() {
   }, [rating]);
 
   useEffect(() => {
-    if (bounds) {
+    if (bounds["sw"] && bounds["ne"]) {
       setIsLoading(true);
       getPlacesData(type, bounds["sw"], bounds["ne"]).then((data) => {
-        setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
         setFilteredPlaces([]);
         setIsLoading(false);
       });
     }
-  }, [type, coordinates, bounds]);
+  }, [type, bounds]);
 
   return (
     <>
